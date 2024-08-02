d5e2570758a5574565488f51a88a2abfcc88cd46
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-02 12:55:59 +0000

    datatypes

---
+++ basics/02_data_type.js
@@ -0,0 +1,27 @@
+"use strict";    // treat all javascript code as newer version
+// alert(1 + 6 + 7)  //we are using node js not browser
+
+//console.log(6 + 7); console.log("hello")    // don't do this it make worst structure of javascript
+
+// write like this. this is redable and clean to understand
+
+console.log(6 + 7) 
+console.log("hello")
+
+let name = "vaibhav"
+let age  = 21
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null )
