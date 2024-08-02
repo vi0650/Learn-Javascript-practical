807aab369dc9192730410e0dfef8129515f8abf5
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-02 12:53:03 +0000

    datatypes

---
+++ learn.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+var accountGmail = "john.doe@gmail.com";
+let accountCity = "chennai";
+
+accountId = 1167;
+accountGmail = "jane.dock@gmail.com";
+accountCity = "Bangalore";
+
+console.table([accountId, accountGmail, accountCity]);
