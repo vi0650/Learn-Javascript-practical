9942974c912c05fde79c82c804f667747af386ec
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-02 15:05:26 +0000

    conversion operation

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -1,4 +1,4 @@
-const accountId = 123456;                       // 1. (const) constant values you can not use this variable again
+ const accountId = 123456;                       // 1. (const) constant values you can not use this variable again
 var accountGmail = "john.doe@gmail.com";        // 2. not preffered to use because of their past problems
 let accountCity = "chennai";                    // 3. use it in project you can change dynamicaly
 let accountName;                                // 4. you can use variable without assign a value - just run it
---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = (false * true)
+
+//console.log(typeof score);
+//console.log(typeof (score));  //parenthesis
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "44" => 45
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "vi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0 => false
+// "" => false
+// "hello" => true
+
+let someNumber = 167
+let strinNumber = String(someNumber)
+console.log(typeof strinNumber);
