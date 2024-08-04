d30f30f389945aa630bed8fe506185ba5d5061dd
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-04 17:19:22 +0000

    operations

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -23,4 +23,31 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 167
 let strinNumber = String(someNumber)
-console.log(typeof strinNumber);
+// console.log(typeof strinNumber);
+
+// ***********************operations**************************///
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = "vaibhav"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(( 3 + 4 ) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num3 = num3 = 2 + 3
+
+let gameCounter = 100
+ gameCounter++;
+console.log(gameCounter);
