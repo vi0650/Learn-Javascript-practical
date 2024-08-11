bca22b55b87850dc904195e47d6d5caf6e83be2a
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-11 13:29:11 +0000

    datatype

---
+++ datatype_summary.js
@@ -0,0 +1,13 @@
+// # primitive datatypes -- 7 catagory
+
+// 7 types : 1.string, 2.number, 3.boolean, 4.null, 5.undefined, 6.symbol, 7.BigInt
+
+const score = 100
+
+const scoreValue = 100.2
+
+// # Reference (Non primitive)
+
+// 1.Array 2.Object 3.Functions
+
+// javascript is dynamicaly typed language
