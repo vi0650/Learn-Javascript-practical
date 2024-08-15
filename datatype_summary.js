5c269cc87f47baf60858ca82b74a45854d918f98
Vaibhav@padmani <106603456+vi0650@users.noreply.github.com>
2024-08-15 16:30:55 +0000

    function

--- datatype_summary.js
+++ datatype_summary.js
@@ -5,11 +5,32 @@
 const score = 100
 
 const scoreValue = 100.2
+const isLoggedIn = true
+const outSideTemp = null
+let userEmail;
 
+const Id = Symbol('1234')
+const anotherId = Symbol('1234')
 
+console.log(Id === anotherId)
+
+const bigNumber = 21847354812845217412n
 
 // # Reference (Non primitive)
 
 // Array, Object, Functions
 
 // javascript is dynamicaly typed language
+
+
+const heros = ["shaktiman", "flying Jatt", "nagraj"];
+let myObj={
+    name: "vaibhav",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+myFunction;
