4e09a8c02696218d40b983e91cb5936a75d02d0a
vaibhav padmani <vaibhavvpadmani650@gmail.com>
2024-08-27 22:54:36 +0530

    String methods

---
+++ basics/04_strings.js
@@ -0,0 +1,10 @@
+const name = "vaibhav"
+const repoCount = 21;
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('wukong','helo') // ? write this line in browser console > press enter > write variable > press enter > you can see lots of string methods in JS tryout this all mehods and learn
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.indexOf('g'));
+console.log(gameName.blink('h'));
